e7d5818a377c177b895fe97e6bfa1ec6961228e0
hwangJN <wlsk401@gmail.com>
2023-07-03 18:39:22 +0900

    seconde commit

--- todos.js
+++ todos.js
@@ -19,17 +19,50 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-function showTodos(todos){
-    console.log();
+function showTodos(str){
+    console.log("show");
+}
+
+function addTodos(name, tags) {
+    console.log("add");
+};
+
+function deleteTodos(id) {
+    console.log("del");
+};
+
+function updateTodos(id, status) {
+    console.log("update");
+};
+
+function parsingWord(str){
+    let command = str.split("$");
+
+    switch(command[0]){
+        case "show":
+            showTodos(command[1]);
+            break;
+        case "add":
+            addTodos(command[1], command[2]);
+            break;
+        case "delete":
+            deleteTodos(command[1]);
+            break;
+        case "update":
+            updateTodos(command[1], command[2]);
+             break;
+        default:
+            break;
+    }
+
+
 }
 
 
 function promptUser() {
-    let input;
+   
     rl.question('명령하세요 : ', (command) => {
-        input = command;
-        //console.log(`현재상태 : ${input}`);
-        showTodos();
+        parsingWord(command);
         promptUser();
     });
 }
