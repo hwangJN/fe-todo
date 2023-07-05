b51ba8c7a92a8571bd4778ed88129807aae4c26b
hwangJN <wlsk401@gmail.com>
2023-07-05 10:25:43 +0900

    [feat] command error try/catch

--- todos.js
+++ todos.js
@@ -11,129 +11,11 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-/*
-
-let next_Id = 3;
-
-function getId() {
-  return next_Id++;
-}
-
-function showStatusCnt() {
-  let outputStr = "현재상태 : ";
-
-  statusCnt.forEach((status) => {
-    outputStr += status.state + " : " + status.cnt + "개, ";
-  });
-
-  console.log(outputStr.slice(0, -2));
-}
-
-function showTodos(status) {
-  if (status === "all") {
-    showStatusCnt();
-  } else {
-    if (!checkValidStatus(status)) {
-      console.log(ERROR.STATUS_ERROR);
-      return;
-    }
-
-    const matchedStatusArr = todos.filter((todo) => todo.status === status);
-
-    console.log(status + "리스트 : 총" + matchedStatusArr.length + "건");
-
-    matchedStatusArr.forEach((todo) => {
-      console.log(todo.name + ", " + todo.id);
-    });
-  }
-}
-
-function addTodos(name, tags) {
-  const newTodoObject = {
-    id: getId(),
-    name: name,
-    tags: tags,
-    status: STATUS.TODO,
-  };
-  todos.push(newTodoObject);
-
-  console.log(name + ` 1개가 추가됐습니다.(id: ${newTodoObject.id})`);
-
-  updateStatusCnt(FLAG.CNT_INCREASE, STATUS.TODO);
-
-  showStatusCnt();
-}
-
-function deleteTodos(id) {
-  const targetIndex = todos.findIndex((item) => item.id == id);
-  if (targetIndex >= 0) {
-    const target = todos[targetIndex];
-    updateStatusCnt(FLAG.CNT_DECREASE, target.status);
-
-    console.log(target.name + ` 가 목록에서 삭제됐습니다.`);
-
-    todos.splice(targetIndex, 1);
-
-    showStatusCnt();
-  } else {
-    console.log(ERROR.ID_ERROR);
-  }
-}
-
-function updateTodos(id, status) {
-  if (!checkValidStatus(status)) {
-    console.log(ERROR.STATUS_ERROR);
-    return;
-  }
-  const targetIndex = todos.findIndex((item) => item.id == id);
-  if (targetIndex >= 0) {
-    const target = todos[targetIndex];
-
-    const newObject = {
-      ...target,
-      status: status,
-    };
-    todos.splice(targetIndex, 1, newObject);
-
-    updateStatusCnt(FLAG.CNT_DECREASE, target.status);
-    updateStatusCnt(FLAG.CNT_INCREASE, status);
-
-    console.log(
-      target.name + ` 가 ${newObject.status}으로 상태가 변경됐습니다.`
-    );
-
-    showStatusCnt();
-  } else {
-    console.log(ERROR.ID_ERROR);
-  }
-}
-
-
-function checkValidStatus(status) {
-  for (let i = 0; i < statusCnt.length; i++) {
-    if (statusCnt[i].state === status) {
-      return true;
-    }
-  }
-  return false;
-}
-
-function updateStatusCnt(flag, status) {
-  if (flag === FLAG.CNT_INCREASE) {
-    const index = statusCnt.findIndex((item) => item.state === status);
-    statusCnt[index].cnt++;
-  } else if (flag === FLAG.CNT_DECREASE) {
-    const index = statusCnt.findIndex((item) => item.state === status);
-    statusCnt[index].cnt--;
-  }
-}
-*/
 
 function checkVaildCommand(commandArr) {
   if (commandArr[0] === COMMAND.SHOW || commandArr[0] === COMMAND.DELETE) {
     if (commandArr.length != 2) {
-      console.log(ERROR.COMMAND_ERROR);
-      return false;
+      throw ERROR.COMMAND_ERROR;
     }
   } else if (
     commandArr[0] === COMMAND.ADD ||
@@ -141,42 +23,47 @@ function checkVaildCommand(commandArr) {
   ) {
     if (commandArr.length != 3) {
       console.log(ERROR.COMMAND_ERROR);
-      return false;
+      throw ERROR.COMMAND_ERROR;
     }
   } else {
-    console.log(ERROR.COMMAND_ERROR);
-    return false;
+    //console.log(ERROR.COMMAND_ERROR);
+    //return false;
+    throw ERROR.COMMAND_ERROR;
   }
-  return true;
 }
 
 function executeCommand(str) {
   const commandArr = str.split("$");
-  if (!checkVaildCommand(commandArr)) {
-    return;
+  // if (!checkVaildCommand(commandArr)) {
+  //   return;
+  // }
+  try {
+    checkVaildCommand(commandArr);
+    switch (commandArr[0]) {
+      case COMMAND.SHOW: //show${status}
+        showTodos(commandArr[1]);
+        break;
+
+      case COMMAND.ADD: //add${name}${tags}
+        addTodos(commandArr[1], commandArr[2]);
+        break;
+
+      case COMMAND.DELETE: //delete${ID}
+        deleteTodos(commandArr[1]);
+        break;
+
+      case COMMAND.UPDATE: //update${ID}${status}
+        updateTodos(commandArr[1], commandArr[2]);
+        break;
+
+      default:
+        console.log(ERROR.COMMAND_ERROR);
+        break;
+    }
+  } catch (e) {
+    console.error(e);
   }
-
-  switch (commandArr[0]) {
-    case COMMAND.SHOW: //show${status}
-      showTodos(commandArr[1]);
-      break;
-
-    case COMMAND.ADD: //add${name}${tags}
-      addTodos(commandArr[1], commandArr[2]);
-      break;
-
-    case COMMAND.DELETE: //delete${ID}
-      deleteTodos(commandArr[1]);
-      break;
-
-    case COMMAND.UPDATE: //update${ID}${status}
-      updateTodos(commandArr[1], commandArr[2]);
-      break;
-
-    default:
-      console.log(ERROR.COMMAND_ERROR);
-      break;
-  }
+  // console.log("ddddddddd");
 }
 
 function promptUser() {
@@ -189,9 +76,9 @@ function promptUser() {
 promptUser();
 
 //가독성
-//모듈화
 //try catch
 
+//모듈화
 //ID
 //상수화
 //naming
